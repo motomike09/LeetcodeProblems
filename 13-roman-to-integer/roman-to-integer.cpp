3cc54b5b0ce62d4cb32ef77d02dfcaaf3e1eef87
Michael Prior <80229699+motomike09@users.noreply.github.com>
2024-02-27 15:25:20 -0700

    Time: 23 ms (7.57%) | Memory: 12.6 MB (38.34%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) 
+    {
+        unordered_map<char, int> RomanSymbolMap = { { 'I' , 1 },
+                                       { 'V' , 5 },
+                                       { 'X' , 10 },
+                                       { 'L' , 50 },
+                                       { 'C' , 100 },
+                                       { 'D' , 500 },
+                                       { 'M' , 1000 } };
+
+       int sum = RomanSymbolMap[s.back()];
+       for (int i = s.length() - 2; i >= 0; --i) 
+       {
+           if (RomanSymbolMap[s[i]] < RomanSymbolMap[s[i + 1]])
+           {
+               sum -= RomanSymbolMap[s[i]];
+           }
+           else
+           {
+               sum += RomanSymbolMap[s[i]];
+           }
+       }
+
+       return sum;
+    }
+};
